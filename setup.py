846ff75ba371fac34f83cdb397b01b1089e8aa60
KwanR <bitkwan@gmail.com>
2017-02-28 13:43:29 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@
 from setuptools import setup
 
 setup(name="ssmanager",
-      version="0.2.3",
+      version="0.2.4",
       description="The python module interact with server-mode shadowsocks.",
-      author="Shell Chen",
+      author="Kwan",
       author_email="orz@sorz.org",
-      url="https://github.com/sorz/ssmanager/",
+      url="https://github.com/bitkwan/ssmanager/",
       packages=['ssmanager'])
 
