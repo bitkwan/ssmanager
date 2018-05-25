d606510c5b98b82efc4aece91f1e234752f7a67d
KwanR <12602991+bitkwan@users.noreply.github.com>
2018-05-25 16:45:49 +0800

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name="ssmanager",
-      version="0.2.5",
+      version="0.2.6",
       description="The python module interact with server-mode shadowsocks.",
       author="Kwan",
       author_email="orz@sorz.org",
